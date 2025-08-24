cb4f2401b7d5a02027952f50aa5fd27067b09423
Nurulshaikh786 <shaikhnurul8200@gmail.com>
2025-08-24 22:22:13 +0530

    First Java Code

---
+++ Calc.java
@@ -0,0 +1,15 @@
+public class Calc {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 40 ;
+        
+        int sum = a + b;
+        int sub = b - a ;
+        int multy = a * b ;
+
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(multy);
+    }
+}
---
+++ Day-1/FirstClass.java
@@ -0,0 +1,8 @@
+class FirstClass{
+    public static void main(String args[]){
+        System.out.println("Hello World");
+        System.out.println("My Name is Nurul");
+        System.out.println("I am a Developer");
+
+    }
+}
